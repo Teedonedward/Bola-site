bd6306fccd600658dc66633b9406380144e0fa0d
Teedon <bola.com.ng123@gmail.com>
2023-12-01 01:31:17 +0100

    modified script

--- scripts/price_compiler.js
+++ scripts/price_compiler.js
@@ -1,14 +1,14 @@
 const quantities = document.querySelectorAll(".quantity");
 const rates = document.querySelectorAll(".rate");
 const prices = document.querySelectorAll(".price");
-const priceWithMoneySign = "$" + rate.value;
+const priceWithMoneySign = "₦" + rate.value;
 
 price.innerHTML = priceWithMoneySign;
 
 quantities.forEach(quantity, i) function() {
-    
+    console.log(quantity);
 }
 
 function compile() {
-    price.innerHTML = "$" + quantity.value * rate.value;
+    price.innerHTML = "₦" + quantity.value[] * rate.value;
 }
