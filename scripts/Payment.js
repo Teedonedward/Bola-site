654767cccc80f2b188edd547124369accdd4e0e0
Teedon <bola.com.ng123@gmail.com>
2024-02-16 00:49:04 +0100

    Integrated paystack payment gateway (test mode)

---
+++ scripts/Payment.js
@@ -0,0 +1,24 @@
+const paymentForm = document.getElementById('order-form');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+
+function payWithPaystack(e) {
+  e.preventDefault();
+
+  let handler = PaystackPop.setup({
+    key: 'pk_test_bb1b40f31f62422df3b268d14c4b414860fd4d43', // Replace with your public key
+    email: document.getElementById("email").value,
+    amount: document.getElementById("amount").value * 100,
+    currency: "NGN",
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+
+  handler.openIframe();
+}
